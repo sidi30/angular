d19acf62bffbd044c4b248432f671eb1cc87e2c6
Ramzi IBRAHIM <sidiramzi11@gmail.com>
2022-01-18 20:05:56 +0100

    commit

---
+++ serveur.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+app.use(express.static('./dist/front'));
+app.get('/*', function (req, res) {
+  res.sendFile("index.html", { root: "/dist/front"});
+});
+app.listen(process.env.PORT || 8080);
